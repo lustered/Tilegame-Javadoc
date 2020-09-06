9a2645ea2fb130b579d9fcf29a160a3e020a287f
lustered <smellslikelavenders@gmail.com>
2020-09-06 12:51:46 -0400

    misc

--- TileGameTester.java
+++ TileGameTester.java
@@ -5,9 +5,12 @@ public class TileGameTester {
     public static void main(String[] args) {
         // TO DO: Code the body of this method
 
-        Object[] options = { "NO", "YES" };
+        // Options for JOptionPane
+        Object[] options = { "No", "Yes" };
         int playAgain;
+        int timesPlayed = 1;
         do {
+            System.out.println("\n\n::Game #: " + timesPlayed + "::\n");
             Hand hand1 = new Hand();
             Hand hand2 = new Hand();
             TileGame game1 = new TileGame(hand1, hand2);
@@ -16,9 +19,11 @@ public class TileGameTester {
             game1.play();
             System.out.println(game1.getResults());
 
+            // Ask user if they want to play the game again
             playAgain = JOptionPane.showOptionDialog(null, "Play again?", "Tile Game", JOptionPane.DEFAULT_OPTION,
                     JOptionPane.WARNING_MESSAGE, null, options, options[0]);
 
+            timesPlayed++;
         } while (playAgain != 0);
 
     }
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/fish
+javac *.java && javar TileGameTester; rm *.class
